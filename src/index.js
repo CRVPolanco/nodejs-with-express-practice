7d965e7b5dba6c6041608687d64e7aaeec153f24
Carlos Vera <carlosvera.cv750@gmail.com>
2022-12-05 23:29:51 -0400

    creating new routes to learn to use them

--- src/index.js
+++ src/index.js
@@ -4,7 +4,19 @@ const port = 3006;
 
 app.get('/', (req, res) =>{
   res.send('Hola servidor en express');
-})
+});
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, esta es una nueva ruta');
+});
+app.get('/products', (req, res) => {
+  res.send('Products');
+});
+app.get('/give-json', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 49.99,
+  })
+});
 app.listen(port, () => {
   console.log('Corriendo en el puerto ' + port);
 })
