4e979eb9d29dd3763b5cec065eb88bbc9b35cd5e
Carlos Vera <carlosvera.cv750@gmail.com>
2022-12-06 11:32:46 -0400

    learning to use the GET method, differents endpoints to get data

--- src/index.js
+++ src/index.js
@@ -2,21 +2,48 @@ const express = require('express');
 const app = express();
 const port = 3006;
 
-app.get('/', (req, res) =>{
-  res.send('Hola servidor en express');
+app.get('/', (req, res) => {
+  console.log(req);
+  res.send('Ruta princiapal');
 });
 app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, esta es una nueva ruta');
 });
 app.get('/products', (req, res) => {
-  res.send('Products');
+  res.json(
+  [
+    {
+      name: 'Product 1',
+      price: 49.99,
+    },
+    {
+      name: 'Product 2',
+      price: 34.99
+    }
+  ]
+  );
 });
-app.get('/give-json', (req, res) => {
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+
   res.json({
-    name: 'Product 1',
-    price: 49.99,
+    id,
+    name: `Product ${id}`,
+    price: 29.99
   })
 });
+
+app.get('/categories/:id/products/:productId', (req, res) => {
+
+  const { id, productId } = req.params;
+
+  res.json({
+    id,
+    productId,
+  })
+
+});
+
 app.listen(port, () => {
   console.log('Corriendo en el puerto ' + port);
-})
+});
