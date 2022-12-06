fe0337241156c54a7c607621e285c0e42471aff8
Carlos Vera <carlosvera.cv750@gmail.com>
2022-12-06 13:31:07 -0400

    learning how to have QUERY PARAMS when routing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const faker = require('faker');
 const app = express();
 const port = 3006;
 
@@ -10,19 +11,25 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, esta es una nueva ruta');
 });
 app.get('/products', (req, res) => {
-  res.json(
-  [
-    {
-      name: 'Product 1',
-      price: 49.99,
-    },
-    {
-      name: 'Product 2',
-      price: 34.99
-    }
-  ]
-  );
+  const products = [];
+
+  const { size } = req.query;
+  const limit = size ?? 100;
+
+  for(let i=0; i<limit; i++){
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+      id: i+1,
+    })
+  }
+
+  res.json(products);
 });
+app.get('/products/filter', (req, res) => {
+  res.send('Soy un filtro, y al estar antes del products/:id yo soy específico y el otro es dinámico')
+})
 app.get('/products/:id', (req, res) => {
   const { id } = req.params;
 
@@ -33,15 +40,24 @@ app.get('/products/:id', (req, res) => {
   })
 });
 
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if(limit && offset){
+    res.json({
+      limit,
+      offset
+    })
+  }else{
+    res.send('No hay parámetros')
+  }
+})
 app.get('/categories/:id/products/:productId', (req, res) => {
-
   const { id, productId } = req.params;
-
   res.json({
     id,
     productId,
   })
-
 });
 
 app.listen(port, () => {
